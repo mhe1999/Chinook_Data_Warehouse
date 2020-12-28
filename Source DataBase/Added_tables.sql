0ad393d073850d79881e6e5c54c3656c7c70a2d3
mohamamd <mohammadsgh98@gmail.com>
2020-12-28 21:52:15 +0330

    add onlinePlayback table and modify customer and track

--- Source DataBase/Added_tables.sql
+++ Source DataBase/Added_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[Rating]
 (
 	[CustomerId] INT NOT NULL,
 	[TrackId] INT NOT NULL,
-	[ScoreDate] DATETIME NOT NULL,
+	[ScoreDate] DATE NOT NULL,
 	[Score] INT NOT NULL,
 	
 	PRIMARY KEY ([CustomerId], [TrackId]),
@@ -10,3 +10,21 @@ CREATE TABLE [dbo].[Rating]
 	FOREIGN KEY([CustomerId]) REFERENCES [dbo].[Customer]([CustomerId]),
     FOREIGN KEY([TrackId]) REFERENCES [dbo].[Track]([TrackId])
 );
+
+
+create TABLE dbo.OnlinePlayback
+(
+	PlayId BIGINT PRIMARY KEY IDENTITY(1,1),
+	CustomerId INT,
+	TrackId INT,
+	PlayDate date,
+	FOREIGN KEY(CustomerId) REFERENCES dbo.Customer (CustomerId),
+	FOREIGN KEY(TrackId) REFERENCES dbo.Track (TrackId),
+);
+
+
+ALTER TABLE Customer
+ADD JoinDate Date DEFAULT '2013-01-01' not null;
+
+ALTER TABLE Track
+ADD AddDate Date DEFAULT '2013-01-01' not null;
