e6f8e8ae7558adfb0c88eab365195da436b30734
mohamamd <mohammadsgh98@gmail.com>
2020-12-30 05:27:12 +0330

    DW: add MediaType ETL procedure

--- Warehouse/ETL.sql
+++ Warehouse/ETL.sql
@@ -1,15 +1,28 @@
-CREATE PROCEDURE ETL_DimensionGenre
+CREATE OR ALTER PROCEDURE ETL_DimensionGenre
 AS
 BEGIN
-
   INSERT INTO [DataWarehouse].[dbo].[Dim_Genre]
   SELECT GenreId, Name
   FROM [StorageArea].[dbo].[SA_Genre]
-  WHERE Name not in (SELECT Name
+  WHERE GenreId not in (SELECT GenreId
   FROM [DataWarehouse].[dbo].[Dim_Genre])
-
-
 END
 
+
+CREATE OR ALTER PROCEDURE ETL_DimensionMediaType
+AS
+BEGIN
+    INSERT INTO [DataWarehouse].[dbo].[Dim_MediaType]
+    SELECT MediaTypeId, Name
+    FROM [StorageArea].[dbo].[SA_MediaType]
+    WHERE MediaTypeId not in (SELECT MediaTypeId
+    FROM [DataWarehouse].[dbo].[Dim_MediaType])
+END
+
+
+
 EXECUTE ETL_DimensionGenre
+EXECUTE ETL_DimensionMediaType
+
 SELECT * FROM [DataWarehouse].[dbo].[Dim_Genre]
+SELECT * FROM [DataWarehouse].[dbo].[Dim_MediaType]
