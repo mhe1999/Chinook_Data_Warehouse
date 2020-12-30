01e1ebe8b02eefd7ce1251855deb1a8e74fc7be8
mohamamd <mohammadsgh98@gmail.com>
2020-12-30 05:31:29 +0330

    DW: add ArtistDimension ETL procedure

--- Warehouse/ETL.sql
+++ Warehouse/ETL.sql
@@ -19,10 +19,21 @@ BEGIN
     FROM [DataWarehouse].[dbo].[Dim_MediaType])
 END
 
+CREATE OR ALTER PROCEDURE ETL_DimensionArtist
+AS
+BEGIN
+    INSERT INTO [DataWarehouse].[dbo].[Dim_Artist]
+    SELECT ArtistId, Name
+    FROM [StorageArea].[dbo].[SA_Artist]
+    WHERE ArtistId not in (SELECT ArtistId
+    FROM [DataWarehouse].[dbo].[Dim_Artist])
+END
 
 
 EXECUTE ETL_DimensionGenre
 EXECUTE ETL_DimensionMediaType
+EXECUTE ETL_DimensionArtist
 
 SELECT * FROM [DataWarehouse].[dbo].[Dim_Genre]
 SELECT * FROM [DataWarehouse].[dbo].[Dim_MediaType]
+SELECT * FROM [DataWarehouse].[dbo].[Dim_Artist]
