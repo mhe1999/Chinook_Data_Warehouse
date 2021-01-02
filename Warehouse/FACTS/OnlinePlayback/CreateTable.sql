6c52cc5352219c79d8e06eacb4f4c1321c056728
mohamamd <mohammadsgh98@gmail.com>
2021-01-02 20:07:46 +0330

    DW: update online playback Transaction Fact ETL Procedures

--- Warehouse/FACTS/OnlinePlayback/CreateTable.sql
+++ Warehouse/FACTS/OnlinePlayback/CreateTable.sql
@@ -1,7 +1,6 @@
 USE [DataWarehouse]
 GO
 
-
 CREATE TABLE [DataWarehouse].[dbo].[FactTransactionOnlinePlayback](
 
     ID BIGINT PRIMARY KEY IDENTITY(1,1),
@@ -12,17 +11,16 @@ CREATE TABLE [DataWarehouse].[dbo].[FactTransactionOnlinePlayback](
     ArtistID INT,
     LocationID INT,
     MediaTypeID INT,
-    TranDate date,-- need to connect dimension, but now temproray worked with source
+    TranDate int,
 
-    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
-    FOREIGN KEY(TrackID) REFERENCES [dbo].[Dim_Track](Id),
-    FOREIGN KEY(AlbumID) REFERENCES [dbo].[Dim_Album](AlbumId),
-    FOREIGN KEY(GenreID) REFERENCES [dbo].[Dim_Genre](GenreId),
-    FOREIGN KEY(ArtistID) REFERENCES [dbo].[Dim_Artist](ArtistId),
-    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
-    FOREIGN KEY(MediaTypeID) REFERENCES [dbo].[Dim_MediaType](MediaTypeId),
-    FOREIGN KEY(CustomerID) REFERENCES [dbo].[Dim_Customer](Id),
---    FOREIGN KEY(TranDate) REFERENCES [dbo].[date](ID)-- date dimension must be create
-
+    FOREIGN KEY(LocationID) REFERENCES [DataWarehouse].[dbo].[Dim_Location](Id),
+    FOREIGN KEY(TrackID) REFERENCES [DataWarehouse].[dbo].[Dim_Track](Id),
+    FOREIGN KEY(AlbumID) REFERENCES [DataWarehouse].[dbo].[Dim_Album](AlbumId),
+    FOREIGN KEY(GenreID) REFERENCES [DataWarehouse].[dbo].[Dim_Genre](GenreId),
+    FOREIGN KEY(ArtistID) REFERENCES [DataWarehouse].[dbo].[Dim_Artist](ArtistId),
+    FOREIGN KEY(LocationID) REFERENCES [DataWarehouse].[dbo].[Dim_Location](Id),
+    FOREIGN KEY(MediaTypeID) REFERENCES [DataWarehouse].[dbo].[Dim_MediaType](MediaTypeId),
+    FOREIGN KEY(CustomerID) REFERENCES [DataWarehouse].[dbo].[Dim_Customer](Id),
+    FOREIGN KEY(TranDate) REFERENCES [DataWarehouse].[dbo].[Dim_Date](TimeKey)
 );
 
--- Warehouse/FACTS/OnlinePlayback/ETL.sql
+++ Warehouse/FACTS/OnlinePlayback/ETL.sql
@@ -1,12 +1,16 @@
+USE [DataWarehouse]
+GO
 ------transaction fact---------
 ------first load---------------
-go
 CREATE OR ALTER PROCEDURE ETL_OP_firstLoadTransFact
 AS
 BEGIN
+
+TRUNCATE TABLE [DataWarehouse].[dbo].[FactTransactionOnlinePlayback];
+
     INSERT INTO [DataWarehouse].[dbo].[FactTransactionOnlinePlayback]
     ( [CustomerID] ,[TrackID], [AlbumID], [GenreID], [ArtistID], [LocationID], [MediaTypeID],[TranDate])
-SELECT DWC.Id, DWT.[Id], DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], SAP.PlayDate
+SELECT DWC.Id, DWT.[Id], DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], DWD.TimeKey
 FROM [StorageArea].[dbo].[SA_Playback] as SAP INNER JOIN [DataWarehouse].[dbo].[Dim_Customer] as DWC ON SAP.CustomerId = DWC.CustomerId AND DWC.Current_Flag_SupportRepId = 1
                                               INNER JOIN [DataWarehouse].[dbo].[Dim_Track] as DWT ON SAP.TrackId = DWT.TrackId AND DWT.Current_Flag_UnitPrice = 1
                                               INNER JOIN [DataWarehouse].[dbo].[Dim_Album] as DWAL ON DWAL.AlbumId = DWT.AlbumId 
@@ -14,7 +18,7 @@ FROM [StorageArea].[dbo].[SA_Playback] as SAP INNER JOIN [DataWarehouse].[dbo].[
                                               INNER JOIN [DataWarehouse].[dbo].Dim_Genre AS DWG ON DWG.GenreId = DWT.GenreId
                                               inner join [DataWarehouse].[dbo].Dim_MediaType AS DWM on DWM.MediaTypeId = DWT.MediaTypeId
                                               inner join [DataWarehouse].[dbo].Dim_Location AS DWL ON DWL.City = DWC.City AND DWL.Country = DWC.Country
-                                                                                              
+                                              INNER JOIN [DataWarehouse].[dbo].Dim_Date AS DWD ON YEAR(DWD.FullDateAlternateKey) =  YEAR(SAP.PlayDate)  AND  MONTH(DWD.FullDateAlternateKey) =  MONTH(SAP.PlayDate)   AND DAY(DWD.FullDateAlternateKey) =  DAY(SAP.PlayDate)                                       
 END
 
 
@@ -24,11 +28,13 @@ CREATE OR ALTER PROCEDURE ETL_OP_incrementalTransFact
 AS
 BEGIN
     DECLARE @EndDate date;
+    DECLARE @TempDATE INT;
     DECLARE @StartDate date;
     DECLARE @CurrDate date;
 
     SET @EndDate = (SELECT MAX(PlayDate) FROM [StorageArea].[dbo].[SA_Playback])
-    SET @StartDate = (SELECT MAX(TranDate) FROM [DataWarehouse].[dbo].[FactTransactionOnlinePlayback]);
+    SET @TempDATE = (SELECT MAX(TranDate) FROM [DataWarehouse].[dbo].[FactTransactionOnlinePlayback]);
+    SET @StartDate = (select DWD.FullDateAlternateKey FROM [DataWarehouse].[dbo].Dim_Date  as DWD WHERE DWD.TimeKey = @TempDATE )
     SET @CurrDate = DATEADD(day, 1, @StartDate);
 
     WHILE @CurrDate <= @EndDate
@@ -36,7 +42,7 @@ BEGIN
 
             INSERT INTO [DataWarehouse].[dbo].[FactTransactionOnlinePlayback]
                 ( [CustomerID] ,[TrackID], [AlbumID], [GenreID], [ArtistID], [LocationID], [MediaTypeID],[TranDate])
-            SELECT DWC.Id, DWT.[Id], DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], SAP.PlayDate
+            SELECT DWC.Id, DWT.[Id], DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], DWD.TimeKey
             FROM [StorageArea].[dbo].[SA_Playback] as SAP INNER JOIN [DataWarehouse].[dbo].[Dim_Customer] as DWC ON SAP.CustomerId = DWC.CustomerId AND DWC.Current_Flag_SupportRepId = 1
                 INNER JOIN [DataWarehouse].[dbo].[Dim_Track] as DWT ON SAP.TrackId = DWT.TrackId AND DWT.Current_Flag_UnitPrice = 1
                 INNER JOIN [DataWarehouse].[dbo].[Dim_Album] as DWAL ON DWAL.AlbumId = DWT.AlbumId
@@ -44,6 +50,7 @@ BEGIN
                 INNER JOIN [DataWarehouse].[dbo].Dim_Genre AS DWG ON DWG.GenreId = DWT.GenreId
                 inner join [DataWarehouse].[dbo].Dim_MediaType AS DWM on DWM.MediaTypeId = DWT.MediaTypeId
                 inner join [DataWarehouse].[dbo].Dim_Location AS DWL ON DWL.City = DWC.City AND DWL.Country = DWC.Country                                                                                             
+                INNER JOIN [DataWarehouse].[dbo].Dim_Date AS DWD ON YEAR(DWD.FullDateAlternateKey) =  YEAR(SAP.PlayDate)  AND  MONTH(DWD.FullDateAlternateKey) =  MONTH(SAP.PlayDate)   AND DAY(DWD.FullDateAlternateKey) =  DAY(SAP.PlayDate)                                       
                 WHERE SAP.PlayDate = @CurrDate
                 SET @CurrDate = DATEADD(day, 1, @Currdate)
         END
