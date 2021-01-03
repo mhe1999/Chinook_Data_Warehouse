1761254d717d3d362c0a4ebba6d46459e04efb3a
MHE <ehteshami1378@gmail.com>
2021-01-03 18:32:51 +0330

    update PlayBack Fact tables

--- Warehouse/FACTS/OnlinePlayback/CreateTable.sql
+++ Warehouse/FACTS/OnlinePlayback/CreateTable.sql
@@ -1,11 +1,14 @@
 USE [DataWarehouse]
 GO
 
+
 CREATE TABLE [DataWarehouse].[dbo].[FactTransactionOnlinePlayback](
 
     ID BIGINT PRIMARY KEY IDENTITY(1,1),
     CustomerID BIGINT,
+--    CustomerIDN INT,
     TrackID BIGINT,
+--    TrackIDN INT,
     AlbumID INT,
     GenreID INT,
     ArtistID INT,
@@ -13,14 +16,73 @@ CREATE TABLE [DataWarehouse].[dbo].[FactTransactionOnlinePlayback](
     MediaTypeID INT,
     TranDate int,
 
-    FOREIGN KEY(LocationID) REFERENCES [DataWarehouse].[dbo].[Dim_Location](Id),
     FOREIGN KEY(TrackID) REFERENCES [DataWarehouse].[dbo].[Dim_Track](Id),
+--    FOREIGN KEY(TrackIDN) REFERENCES [DataWarehouse].[dbo].[Dim_Track](TrackId),
     FOREIGN KEY(AlbumID) REFERENCES [DataWarehouse].[dbo].[Dim_Album](AlbumId),
     FOREIGN KEY(GenreID) REFERENCES [DataWarehouse].[dbo].[Dim_Genre](GenreId),
     FOREIGN KEY(ArtistID) REFERENCES [DataWarehouse].[dbo].[Dim_Artist](ArtistId),
     FOREIGN KEY(LocationID) REFERENCES [DataWarehouse].[dbo].[Dim_Location](Id),
     FOREIGN KEY(MediaTypeID) REFERENCES [DataWarehouse].[dbo].[Dim_MediaType](MediaTypeId),
     FOREIGN KEY(CustomerID) REFERENCES [DataWarehouse].[dbo].[Dim_Customer](Id),
+--    FOREIGN KEY(CustomerIDN) REFERENCES [DataWarehouse].[dbo].[Dim_Customer](CustomerId),
     FOREIGN KEY(TranDate) REFERENCES [DataWarehouse].[dbo].[Dim_Date](TimeKey)
 );
 
+CREATE TABLE dbo.FactDailySnapshotOnlinePlayback --in this periodic table we investigate **Track** statistics
+(
+
+    TrackID BIGINT,
+    AlbumID INT,
+    GenreID INT,
+    ArtistID INT,
+    LocationID INT,
+    MediaTypeID INT,
+    TranDate INT,
+    PRIMARY KEY(TrackID, TranDate, LocationID),
+    -- measures
+    NumberOfPlaybackToday INT,
+    MaxNum INT,
+    MinNum INT,
+    NumberOfPlaybackUntillToday INT,-- today + past days
+    AverageNumOfPlaybackUntillToday INT,-- today + past days
+
+
+    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
+    FOREIGN KEY(TrackID) REFERENCES [dbo].[Dim_Track](Id),
+    FOREIGN KEY(AlbumID) REFERENCES [dbo].[Dim_Album](AlbumId),
+    FOREIGN KEY(GenreID) REFERENCES [dbo].[Dim_Genre](GenreId),
+    FOREIGN KEY(ArtistID) REFERENCES [dbo].[Dim_Artist](ArtistId),
+    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
+    FOREIGN KEY(MediaTypeID) REFERENCES [dbo].[Dim_MediaType](MediaTypeId),
+    FOREIGN KEY(TranDate) REFERENCES [dbo].[Dim_Date](TimeKey)-- date dimension must be create
+
+);
+
+CREATE TABLE dbo.FactACCOnlinePlayback --ACC Fact table
+(
+
+    TrackID BIGINT,
+    AlbumID INT,
+    GenreID INT,
+    ArtistID INT,
+    LocationID INT,
+    MediaTypeID INT,
+    NumberOfTracks INT,
+    PRIMARY KEY(TrackID, LocationID),
+
+    -- measures
+    MaxNum INT,
+    MinNum INT,
+    NumberOfPlayback INT,
+    AverageNumOfPlayback INT,
+
+
+    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
+    FOREIGN KEY(TrackID) REFERENCES [dbo].[Dim_Track](Id),
+    FOREIGN KEY(AlbumID) REFERENCES [dbo].[Dim_Album](AlbumId),
+    FOREIGN KEY(GenreID) REFERENCES [dbo].[Dim_Genre](GenreId),
+    FOREIGN KEY(ArtistID) REFERENCES [dbo].[Dim_Artist](ArtistId),
+    FOREIGN KEY(LocationID) REFERENCES [dbo].[Dim_Location](Id),
+    FOREIGN KEY(MediaTypeID) REFERENCES [dbo].[Dim_MediaType](MediaTypeId),
+
+);
