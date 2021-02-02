5d67636f5a626bcaf02935575bc1e88abf45c930
mohamamd <mohammadsgh98@gmail.com>
2021-02-02 16:00:39 +0330

    DW: rating log

--- Warehouse/FACTS/Rating/ETL.sql
+++ Warehouse/FACTS/Rating/ETL.sql
@@ -8,6 +8,10 @@ BEGIN
 
     TRUNCATE TABLE [DataWarehouse].[dbo].[FactTransactionRating];
 
+    INSERT into [DataWarehouse].[dbo].[LogTable]
+    VALUES
+        ('[DataWarehouse].[dbo].[FactTransactionRating]', 'TRUNCATE for first load', NULL, GETDATE())
+
     DECLARE @EndDate date;
     DECLARE @CurrDate date;
 
@@ -18,24 +22,24 @@ BEGIN
     WHILE @CurrDate <= @EndDate
     BEGIN
 
-    INSERT INTO [DataWarehouse].[dbo].[FactTransactionRating]
-        ( [CustomerID], [CustomerIDN], [TrackID], [TrackIDN], [AlbumID], [GenreID], [ArtistID], [LocationID], [MediaTypeID],[TranDate], Score)
-    SELECT DWC.Id, DWC.CustomerId, DWT.[Id], DWT.TrackId, DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], DWD.TimeKey, SAP.Score
-    FROM [StorageArea].[dbo].[SA_Rating] as SAP INNER JOIN [DataWarehouse].[dbo].[Dim_Customer] as DWC ON SAP.CustomerId = DWC.CustomerId AND DWC.Current_Flag_SupportRepId = 1
-        INNER JOIN [DataWarehouse].[dbo].[Dim_Track] as DWT ON SAP.TrackId = DWT.TrackId AND DWT.Current_Flag_UnitPrice = 1
-        INNER JOIN [DataWarehouse].[dbo].[Dim_Album] as DWAL ON DWAL.AlbumId = DWT.AlbumId
-        INNER JOIN [DataWarehouse].[dbo].[Dim_Artist] AS DWAR ON DWAR.ArtistId = DWT.ArtistId
-        INNER JOIN [DataWarehouse].[dbo].Dim_Genre AS DWG ON DWG.GenreId = DWT.GenreId
-        inner join [DataWarehouse].[dbo].Dim_MediaType AS DWM on DWM.MediaTypeId = DWT.MediaTypeId
-        inner join [DataWarehouse].[dbo].Dim_Location AS DWL ON DWL.City = DWC.City AND DWL.Country = DWC.Country
-        INNER JOIN [DataWarehouse].[dbo].Dim_Date AS DWD ON YEAR(DWD.FullDateAlternateKey) =  YEAR(SAP.ScoreDate) AND MONTH(DWD.FullDateAlternateKey) =  MONTH(SAP.ScoreDate) AND DAY(DWD.FullDateAlternateKey) =  DAY(SAP.ScoreDate)
-        WHERE SAP.ScoreDate = @CurrDate
+        INSERT INTO [DataWarehouse].[dbo].[FactTransactionRating]
+            ( [CustomerID], [CustomerIDN], [TrackID], [TrackIDN], [AlbumID], [GenreID], [ArtistID], [LocationID], [MediaTypeID],[TranDate], Score)
+        SELECT DWC.Id, DWC.CustomerId, DWT.[Id], DWT.TrackId, DWAL.[AlbumId], DWG.[GenreId], [DWAR].[ArtistId], [DWL].Id, [DWM].[MediaTypeId], DWD.TimeKey, SAP.Score
+        FROM [StorageArea].[dbo].[SA_Rating] as SAP INNER JOIN [DataWarehouse].[dbo].[Dim_Customer] as DWC ON SAP.CustomerId = DWC.CustomerId AND DWC.Current_Flag_SupportRepId = 1
+            INNER JOIN [DataWarehouse].[dbo].[Dim_Track] as DWT ON SAP.TrackId = DWT.TrackId AND DWT.Current_Flag_UnitPrice = 1
+            INNER JOIN [DataWarehouse].[dbo].[Dim_Album] as DWAL ON DWAL.AlbumId = DWT.AlbumId
+            INNER JOIN [DataWarehouse].[dbo].[Dim_Artist] AS DWAR ON DWAR.ArtistId = DWT.ArtistId
+            INNER JOIN [DataWarehouse].[dbo].Dim_Genre AS DWG ON DWG.GenreId = DWT.GenreId
+            inner join [DataWarehouse].[dbo].Dim_MediaType AS DWM on DWM.MediaTypeId = DWT.MediaTypeId
+            inner join [DataWarehouse].[dbo].Dim_Location AS DWL ON DWL.City = DWC.City AND DWL.Country = DWC.Country
+            INNER JOIN [DataWarehouse].[dbo].Dim_Date AS DWD ON YEAR(DWD.FullDateAlternateKey) =  YEAR(SAP.ScoreDate) AND MONTH(DWD.FullDateAlternateKey) =  MONTH(SAP.ScoreDate) AND DAY(DWD.FullDateAlternateKey) =  DAY(SAP.ScoreDate)
+            WHERE SAP.ScoreDate = @CurrDate
 
-    SET @CurrDate = DATEADD(day, 1, @Currdate)
-/*
-    INSERT into [DataWarehouse].[dbo].[LogTable]
-    VALUES
-        ('[DataWarehouse].[dbo].[FactTransactionOnlinePlayback]', 'FirstLoad, joined from Dim_Customer, Dim_Track, Dim_Album, Dim_Artist, Dim_Genre, Dim_MediaType, Dim_Location, Dim_Date', NULL, GETDATE())*/
+        INSERT into [DataWarehouse].[dbo].[LogTable]
+        VALUES
+            ('[DataWarehouse].[dbo].[FactTransactionRating]', 'FirstLoad, joined from Dim_Customer, Dim_Track, Dim_Album, Dim_Artist, Dim_Genre, Dim_MediaType, Dim_Location, Dim_Date', @CurrDate, GETDATE())
+
+        SET @CurrDate = DATEADD(day, 1, @Currdate)
 
     END
 END
@@ -76,8 +80,8 @@ BEGIN
                 inner join [DataWarehouse].[dbo].Dim_Location AS DWL ON DWL.City = DWC.City AND DWL.Country = DWC.Country
                 INNER JOIN [DataWarehouse].[dbo].Dim_Date AS DWD ON YEAR(DWD.FullDateAlternateKey) =  YEAR(SAP.ScoreDate) AND MONTH(DWD.FullDateAlternateKey) =  MONTH(SAP.ScoreDate) AND DAY(DWD.FullDateAlternateKey) =  DAY(SAP.ScoreDate)
                 WHERE SAP.ScoreDate = @CurrDate
-            /*        INSERT into [DataWarehouse].[dbo].[LogTable]
-                    VALUES('[DataWarehouse].[dbo].[FactTransactionOnlinePlayback]', 'Incremental Load, joined from Dim_Customer, Dim_Track, Dim_Album, Dim_Artist, Dim_Genre, Dim_MediaType, Dim_Location, Dim_Date', @Currdate, GETDATE())*/
+                    INSERT into [DataWarehouse].[dbo].[LogTable]
+                    VALUES('[DataWarehouse].[dbo].[FactTransactionRating]', 'Incremental Load, joined from Dim_Customer, Dim_Track, Dim_Album, Dim_Artist, Dim_Genre, Dim_MediaType, Dim_Location, Dim_Date', @Currdate, GETDATE())
         
         SET @CurrDate = DATEADD(day, 1, @Currdate)
         END
@@ -191,10 +195,23 @@ BEGIN
     SET @TempDate = (select max(TranDate)
     from [DataWarehouse].[dbo].[FactDailySnapshotRating])
     TRUNCATE TABLE [DataWarehouse].[dbo].[FactACCRating]
+    INSERT into [DataWarehouse].[dbo].[LogTable]
+    VALUES('[DataWarehouse].[dbo].[FactACCRating]', 'TRUNCATE ACC fact', (select FullDateAlternateKey
+            from Dim_Date
+            where TimeKey= @TempDate), GETDATE())
+
+ 
     INSERT INTO [DataWarehouse].[dbo].[FactACCRating]
     SELECT TrackID, AlbumID, GenreID, ArtistID, LocationID, MediaTypeID, Track_AVG_Score, Number_Of_Votes_untillToday
     FROM [DataWarehouse].[dbo].[FactDailySnapshotRating]
     WHERE TranDate = @TempDate
+
+    INSERT into [DataWarehouse].[dbo].[LogTable]
+    VALUES('[DataWarehouse].[dbo].[FactACCRating]', 'Load data to ACC fact', (select FullDateAlternateKey
+            from Dim_Date
+            where TimeKey= @TempDate), GETDATE())
+
+
 END
 
 
